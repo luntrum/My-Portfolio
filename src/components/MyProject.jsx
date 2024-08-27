e6f548b3682dda58d3f63362eac8e30d80285c9e
loc nguyen <nguyenhuuloc.0705@gmail.com>
2024-08-27 16:55:42 +0700

    update MyProject description

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -14,13 +14,31 @@ function MyProject() {
           <img
             src="./img/jobmanagerProject.png"
             alt="job manager project"
-            className="w-3/4 md:w-1/2 mx-auto "
+            className="w-3/4 md:w-1/2 mx-auto  "
           />
         </a>
       </div>
       <p className="dark:text-white w-4/7 mx-auto   text-xl  italic">
         A practical project designed to sharpen my skills, featuring intuitive
-        job management functionality. Check it out!
+        job management functionality.{' '}
+        <a
+          href="https://job-management-ujed.onrender.com/"
+          target="_blank"
+          className="underline"
+        >
+          Check it out!
+        </a>
+      </p>
+      <p className="dark:text-white w-4/7 mx-auto   text-xl  italic  ml-0 md:ml-auto  ">
+        You can visit{' '}
+        <a
+          href="https://github.com/luntrum"
+          target="_blank"
+          className=" underline font-bold "
+        >
+          my github
+        </a>{' '}
+        for more.
       </p>
     </section>
   );
